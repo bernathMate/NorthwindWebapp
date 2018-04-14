4e90ad72a0206a39b129f9c36958cd881efc5db8
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-14 08:26:01 +0200

    Task3Servlet is implemented, doGet method is overridden.

--- src/main/java/com/codecool/web/servlet/Task3Servlet.java
+++ src/main/java/com/codecool/web/servlet/Task3Servlet.java
@@ -1,4 +1,44 @@
 package com.codecool.web.servlet;
 
-public class Task3Servlet {
+import com.codecool.web.dao.TaskDao;
+import com.codecool.web.dao.database.DatabaseTaskDao;
+import com.codecool.web.model.Task3;
+import com.codecool.web.service.TaskService;
+import com.codecool.web.service.exception.ServiceException;
+import com.codecool.web.service.simple.SimpleTaskService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/task3")
+public class Task3Servlet extends AbstractServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String companyName = req.getParameter("companyName");
+        try (Connection connection = getConnection(req.getServletContext())) {
+            TaskDao taskDao = new DatabaseTaskDao(connection);
+            TaskService taskService = new SimpleTaskService(taskDao);
+
+            if (companyName == null || companyName.equals("")) {
+                List<Task3> task3Result = taskService.getTask3results();
+                req.setAttribute("task3", task3Result);
+            } else {
+                List<Task3> task3FilterResult = taskService.getCompanyByName(companyName);
+                req.setAttribute("task3", task3FilterResult);
+            }
+
+            req.getRequestDispatcher("task3.jsp").forward(req, resp);
+        } catch (SQLException e) {
+            req.setAttribute("error", e.getMessage());
+        } catch (ServiceException e) {
+            req.setAttribute("error", e.getMessage());
+        }
+    }
 }
