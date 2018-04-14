f573ed9d40573300ca803bb64e8362055f8e0456
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-14 08:19:47 +0200

    Task4 class is implemented with field, constructor, and getter

--- src/main/java/com/codecool/web/model/Task4.java
+++ src/main/java/com/codecool/web/model/Task4.java
@@ -1,4 +1,32 @@
 package com.codecool.web.model;
 
-public class Task4 {
+import java.util.Objects;
+
+public class Task4 extends AbstractModel {
+
+    private String ids;
+
+    public Task4(String company, String ids) {
+        super(company);
+        this.ids = ids;
+    }
+
+    public String getIds() {
+        return ids;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Task4 task4 = (Task4) o;
+        return Objects.equals(ids, task4.ids);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(super.hashCode(), ids);
+    }
 }
