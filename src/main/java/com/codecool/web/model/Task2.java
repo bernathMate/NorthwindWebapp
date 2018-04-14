636cbf7f1ca5bd6488bc8cae70a457e4a961e031
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-14 08:18:54 +0200

    Task2 class is implemented with fields, constructor, and getters

--- src/main/java/com/codecool/web/model/Task2.java
+++ src/main/java/com/codecool/web/model/Task2.java
@@ -1,4 +1,32 @@
 package com.codecool.web.model;
 
-public class Task2 {
+import java.util.Objects;
+
+public class Task2 extends AbstractModel {
+
+    private int products;
+
+    public Task2(String company, int products) {
+        super(company);
+        this.products = products;
+    }
+
+    public int getProducts() {
+        return products;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Task2 task2 = (Task2) o;
+        return products == task2.products;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(super.hashCode(), products);
+    }
 }
