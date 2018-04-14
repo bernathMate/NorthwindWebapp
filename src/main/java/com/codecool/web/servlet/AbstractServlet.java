688641909e14c73479942272571b672df4ae27cb
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-14 08:24:36 +0200

    AbstractServlet is implemented, it has a getConnection method, this method will return a Connection object. Each Servlet will extends from this class.

--- src/main/java/com/codecool/web/servlet/AbstractServlet.java
+++ src/main/java/com/codecool/web/servlet/AbstractServlet.java
@@ -1,4 +1,15 @@
 package com.codecool.web.servlet;
 
-public class AbstractServlet {
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+abstract class AbstractServlet extends HttpServlet {
+
+    Connection getConnection(ServletContext sce) throws SQLException {
+        DataSource dataSource = (DataSource) sce.getAttribute("dataSource");
+        return dataSource.getConnection();
+    }
 }
