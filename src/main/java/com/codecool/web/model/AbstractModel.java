bbad1ff5b45df262fab78a451e85744ea2642aac
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-14 08:16:56 +0200

    AbstractModel class is implemented, this class contains a company (String) field, because every task has a company column

--- src/main/java/com/codecool/web/model/AbstractModel.java
+++ src/main/java/com/codecool/web/model/AbstractModel.java
@@ -1,4 +1,30 @@
 package com.codecool.web.model;
 
-public class AbstractModel {
+import java.util.Objects;
+
+public abstract class AbstractModel {
+
+    private String company;
+
+    public AbstractModel(String company) {
+        this.company = company;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractModel that = (AbstractModel) o;
+        return Objects.equals(company, that.company);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(company);
+    }
 }
