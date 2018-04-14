0b82716800eacf9155c4f6a67a63a939092fb17d
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-14 08:20:13 +0200

    Task5 class is implemented with field, constructor, and getters

--- src/main/java/com/codecool/web/model/Task5.java
+++ src/main/java/com/codecool/web/model/Task5.java
@@ -1,4 +1,39 @@
 package com.codecool.web.model;
 
-public class Task5 {
+import java.util.Objects;
+
+public class Task5 extends AbstractModel {
+
+    private String product;
+    private double price;
+
+    public Task5(String company, String product, double price) {
+        super(company);
+        this.product = product;
+        this.price = price;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Task5 task5 = (Task5) o;
+        return Double.compare(task5.price, price) == 0 &&
+                Objects.equals(product, task5.product);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(super.hashCode(), product, price);
+    }
 }
