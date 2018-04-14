fde5dfb03dcfcda93943a882981162e62a5e763a
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-14 08:13:22 +0200

    AbstractDao class is implemented, this class contains a connection object

--- src/main/java/com/codecool/web/dao/database/AbstractDao.java
+++ src/main/java/com/codecool/web/dao/database/AbstractDao.java
@@ -1,4 +1,12 @@
 package com.codecool.web.dao.database;
 
-public class AbstractDao {
+import java.sql.Connection;
+
+public abstract class AbstractDao {
+
+    final Connection connection;
+
+    public AbstractDao(Connection connection) {
+        this.connection = connection;
+    }
 }
