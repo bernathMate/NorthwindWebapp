871d7abc6521db5def18838998b552d1b120a7c9
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-14 08:18:38 +0200

    Task1 class is implemented with fields, constructor, and getter

--- src/main/java/com/codecool/web/model/Task1.java
+++ src/main/java/com/codecool/web/model/Task1.java
@@ -1,4 +1,32 @@
 package com.codecool.web.model;
 
-public class Task1 {
+import java.util.Objects;
+
+public class Task1 extends AbstractModel {
+
+    private String product;
+
+    public Task1(String company,  String product) {
+        super(company);
+        this.product = product;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Task1 task1 = (Task1) o;
+        return Objects.equals(product, task1.product);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(super.hashCode(), product);
+    }
 }
